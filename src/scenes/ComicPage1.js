803c7d5f1e97f92f02cda71f2530ed8bd91f398c
NongFai <narisara.li@mail.kmutt.ac.th>
2019-12-24 17:30:37 +0700

    [UPDATE] sound pageComicPg1+MainMenu

--- src/scenes/ComicPage1.js
+++ src/scenes/ComicPage1.js
@@ -15,37 +15,17 @@ class ComicPage1 extends Phaser.Scene{
     preload(){
         this.load.image('comicPg1','src/images/comicP1.png')
         this.load.image('nextbtn','src/images/nextButton.png')
-        //this.load.audio('comic_song','src/songs/Heartbeats - Broove (LuviKunG Remix).mp3')
+        this.load.audio('comic_song','src/songs/Heartbeats - Broove (LuviKunG Remix).mp3')
     }
     
     create(){
         comicPg1 = this.add.image(0, 0,'comicPg1').setScale(0.7).setOrigin(0,0)
         nextbtn = this.add.image(550, 750, 'nextbtn').setScale(0.2).setInteractive()
         comic_song = this.sound.add('comic_song')
-        //comic_song.play()
-        // this.input.addDownCallback(
-        //     function() { 
-        //         console.group('game.sound.context.state');
-        //         console.log(game)
-        //         console.groupEnd('game.sound.context.state');
-        //         if (game.sound.context.state === 'suspended') { 
-        //             game.sound.context.resume(); 
-        //         } 
-        //     });
-        // event = this.time.addEvent({
-        //     delay: 100,
-        //     callback: function () {
-        //         if (game.sound.context.state === 'suspended') { 
-        //                         game.sound.context.resume(); 
-        //                     } 
-
-        //     },
-        //     callbackScope: this,
-        //     loop: true,
-        //     paused: false,
-        // })
+        comic_song.play()
+ 
         nextbtn.on('pointerdown', (pointer) =>{
-            //comic_song.stop()
+            comic_song.stop()
             this.goMainMenu()
         })
     }
--- src/scenes/MainMenu.js
+++ src/scenes/MainMenu.js
@@ -5,6 +5,7 @@ let gameName
 let howToPlay
 let storyMode
 let arcadeMode
+let MainMenu_song
 
 class MainMenu extends Phaser.Scene {
 
@@ -20,6 +21,7 @@ class MainMenu extends Phaser.Scene {
         this.load.image('howToPlay', 'src/images/HowToPlay.png')
         this.load.image('storyM', 'src/images/StoryMode.png')
         this.load.image('arcadeM', 'src/images/ArcadeMode.png')
+        this.load.audio('MainMenu_song','src/songs/Heartbeats - Broove (LuviKunG Remix).mp3')
 
     }
 
@@ -29,9 +31,12 @@ class MainMenu extends Phaser.Scene {
         howToPlay = this.add.image(350, 450, 'howToPlay').setScale(0.2)
         storyMode = this.add.image(300, 700, 'storyM').setScale(0.5)
         arcadeMode = this.add.image(500, 700, 'arcadeM').setScale(0.5)
+        MainMenu_song = this.sound.add('MainMenu_song')
+        MainMenu_song.play()
 
         storyMode.setInteractive()
         storyMode.on('pointerdown', (pointer) => {
+            MainMenu_song.stop()
             this.goStoryWave1()
         });
     
