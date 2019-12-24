558d66925ffdc7d1b0da7e85d6f64fa618767fa1
NongFai <narisara.li@mail.kmutt.ac.th>
2019-12-24 14:44:12 +0700

    [UPDATE] MainMenu

--- src/scenes/MainMenu.js
+++ src/scenes/MainMenu.js
@@ -4,33 +4,52 @@ let howToPlay
 let storyMode
 let arcadeMode
 
-class MainMenu extends Phaser.Scene{
+class MainMenu extends Phaser.Scene {
 
-    constructor(){
+    constructor() {
         super({
             key: 'MainMenu'
         })
     }
 
-    preload(){
-        this.load.image('bg','src/images/BG.jpg')
-        this.load.image('gameName','src/images/GameName.png')
-        this.load.image('howToPlay','src/images/HowToPlay.png')
-        this.load.image('storyM','src/images/StoryMode.png')
-        this.load.image('arcadeM','src/images/ArcadeMode.png')
-        
-    }
-    
-    create(){
-        background = this.add.image(600, 800,'bg').setOrigin(0,0)
-        gameName = this.add.image(0, 0, 'gameName').setScale(0.6)
-        howToPlay = this.add.image(0, 0,'howToPlay').setScale(0.2)
-        storyMode = this.add.image(0, 0, 'storyM').setScale(0.5)
-        arcadeMode = this.add.image(0, 0,'arcadeM').setScale(0.5)
+    preload() {
+        this.load.image('bg', 'src/images/BG.jpg')
+        this.load.image('gameName', 'src/images/GameName.png')
+        this.load.image('howToPlay', 'src/images/HowToPlay.png')
+        this.load.image('storyM', 'src/images/StoryMode.png')
+        this.load.image('arcadeM', 'src/images/ArcadeMode.png')
+
     }
 
-    update(delta, time){
-         background.tilePositionY-=1
+    create() {
+        background = this.add.image(0, 0, 'bg').setOrigin(0, 0).setSize(600, 800).setScale(0.6)
+        gameName = this.add.image(250, 150, 'gameName').setScale(0.6)
+        howToPlay = this.add.image(350, 450, 'howToPlay').setScale(0.2)
+        storyMode = this.add.image(300, 700, 'storyM').setScale(0.5)
+        arcadeMode = this.add.image(500, 700, 'arcadeM').setScale(0.5)
+
+        storyMode.setInteractive()
+        storyMode.on('pointerdown', (pointer) => {
+            //console.log('pointerdown')
+            storyMode.setTint(0x00ff00)
+            this.goStoryWave1()
+        });
+        storyMode.on('pointerup', (pointer) => {
+            //console.log('pointerup')
+            storyMode.clearTint()
+        });
+    }
+
+    goStoryWave1() {
+        this.goToScene('GameScene')
+    }
+
+    goToScene(scene){
+        this.scene.start(scene)
+    }
+
+    update(delta, time) {
+
     }
 
 }
