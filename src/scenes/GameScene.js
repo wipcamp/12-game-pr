2e0c2b388fa79aa5c9ff54588b865b4a31636560
Jednuth <jednuth.tatirat@mail.kmutt.ac.th>
2019-12-24 15:08:26 +0700

    [ADD] enemy fix

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,3 +1,8 @@
+import Enemy from './core/Enemy'
+
+let enemyGroup
+let enemyKey = 'enemy'
+let enemy
 class GameScene extends Phaser.Scene{
 
     constructor(){
@@ -7,10 +12,16 @@ class GameScene extends Phaser.Scene{
     }
 
     preload(){
-        
+        this.load.image(enemyKey,'src/images/flyMan_stand.png', { frameWidth: 122, frameHeight: 139 })
     }
     
     create(){
+        //  enemy = new Enemy(this, 500, 200,enemyKey);
+        // enemy.setObjectWorldBounds();
+        // enemy.setSize(0.2)
+        enemyGroup = this.physics.add.group();
+        enemy.setEnemyGroup()
+
 
     }
 
--- src/scenes/core/Enemy.js
+++ src/scenes/core/Enemy.js
@@ -9,6 +9,30 @@ export default class Enemy extends Phaser.GameObjects.Sprite{
 
       this.setData("speed", 200);
     }
-    
+
+    setEnemyGroup(){
+        
+        event = this.time.addEvent({
+            delay : 2000,
+            callback : function (){
+            enemy = this.physics.add.sprite(Phaser.Math.Between(0,600),20,'enemyKey').setScale(0.1)
+            enemyGroup.add(enemy)
+            enemy.setVelocityY(200)
+            
+            },
+            callbackScope :this,
+            loop : true,
+            pause : false,
+            timeScale:1,
+        })
+    }
+
+    setObjectWorldBounds(){
+        this.body.setCollideWorldBounds(true);
+    }
+     
+    setSize(size){
+        this.setScale(size)
+    }
 
 }
