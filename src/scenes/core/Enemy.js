3a9c942e5516b9ca4a081726ac5ff7f016836907
Jednuth <jednuth.tatirat@mail.kmutt.ac.th>
2019-12-24 14:36:58 +0700

    [ADD] enemy

--- src/scenes/core/Enemy.js
+++ src/scenes/core/Enemy.js
@@ -1,15 +1,14 @@
 import 'Phaser';
-
-export default class Player extends ObjectWipcamp{
+export default class Enemy extends Phaser.GameObjects.Sprite{
     constructor(scene, x, y, key) {
       super(scene, x, y, key);
-    this.key = key;
+      this.key = key;
       this.scene = scene;
       this.scene.add.existing(this);
       this.scene.physics.world.enableBody(this, 0);
 
       this.setData("speed", 200);
-      
-
     }
+    
+
 }
