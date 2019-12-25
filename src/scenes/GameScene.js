a1048c1170ae4a058d3608a9c894b646fc3a3bf7
NongFai <narisara.li@mail.kmutt.ac.th>
2019-12-25 20:32:20 +0700

    [ADD] Destroy player's bullet byNongFai

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -2,41 +2,48 @@ import Player from './core/Player'
 let player;
 let playerKey = 'player';
 let bulletKey = 'bullet';
-class GameScene extends Phaser.Scene{
-    constructor(){
+let zone;
+
+class GameScene extends Phaser.Scene {
+    constructor() {
         super({
             key: 'GameScene'
         })
     }
 
-    preload(){
-        this.load.image(playerKey,'src/images/Gokuตัดเองจ้า.png')
-        this.load.spritesheet(bulletKey,'src/image/character.png')
+    preload() {
+        this.load.image(playerKey, 'src/images/Gokuตัดเองจ้า.png')
+        this.load.spritesheet(bulletKey, 'src/image/character.png')
     }
-    
-    create(){
+
+    create() {
+        
         player = new Player(
             this,
-            300, 750,playerKey
+            300, 750, playerKey
         )
         console.log(player);
         this.keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
         this.keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
-        
-        player.areShooting(bulletKey,player)
+
+        player.areShooting(bulletKey, player)
 
         player.setWorldBound()
-        
+
         player.setSize(0.1)
 
         player.setHitBox()
 
         player.setoffset()
-        
+
         // player.stopShooting()
+        
+      
     }
 
-    update(delta, time){
+
+
+    update(delta, time) {
         if (this.keyA.isDown) {
             player.moveLeft();
         }
@@ -46,6 +53,8 @@ class GameScene extends Phaser.Scene{
             player.body.velocity.x = 0;
             player.body.velocity.y = 0;
         }
+
+        
     }
 
 }
