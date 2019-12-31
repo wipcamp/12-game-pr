737f5b8a228ecb8b871f3b9c00f174713bd581b3
Sharkthoven <phichitphol.liszt@mail.kmutt.ac.th>
2019-12-31 20:32:01 +0700

    [ADD] Enemy touch player ==> Enemy Destroy

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -7,6 +7,7 @@ let player;
 let playerKey = 'player';
 let bulletKey = 'bullet';
 let heart
+let playerHealth
 let zone;
 
 class GameScene extends Phaser.Scene {
@@ -16,28 +17,30 @@ class GameScene extends Phaser.Scene {
         })
     }
 
-    preload() {
-        this.load.image(playerKey, 'src/images/Gokuตัดเองจ้า.png')
-        this.load.spritesheet(bulletKey, 'src/image/character.png')
+    preload(){
+        this.load.image(playerKey,'src/images/Gokuตัดเองจ้า.png')
+        this.load.spritesheet(bulletKey,'src/image/character.png')
         this.load.image('heart', 'src/images/Heart.png')
         this.load.image(enemyKey,'src/images/flyMan_stand.png', { frameWidth: 122, frameHeight: 139 })
     }
+    
+    create(){
 
-    create() {
-        
         heart = this.add.image(585, 20,'heart').setScale(0.5)
         heart = this.add.image(549, 20,'heart').setScale(0.5)
         heart = this.add.image(513, 20,'heart').setScale(0.5)
-        
+
         player = new Player(
             this,
             300, 750, playerKey
         )
-        console.log(player);
+        // console.log(player);
         this.keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
         this.keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+        
+        player.areShooting(bulletKey,player)
 
-        player.areShooting(bulletKey, player)
+        // player.areShooting(bulletKey, player)
 
         player.setWorldBound()
 
@@ -49,17 +52,17 @@ class GameScene extends Phaser.Scene {
 
     enemyGroup = this.physics.add.group();
     
-    
-    
-   
-   
      event = this.time.addEvent({
             delay : 2000,
             callback : function (){
             //  let enemy = new Enemy(this,500,200,enemyKey)
-            enemyGroup = this.physics.add.image(Phaser.Math.Between(0,600),20,enemyKey).setScale(0.1)
+            //enemyGroup = this.physics.add.image(Phaser.Math.Between(0,600),20,enemyKey).setScale(0.1)
+            let enemy = this.physics.add.image(Phaser.Math.Between(0,600),20,enemyKey)
+            enemy.setScale(0.1)
+            enemyGroup.add(enemy)
             enemyGroup.setVelocityY(500)
-            this.physics.moveToObject(enemyGroup,player,300)
+            for(let i = 0 ;i < enemyGroup.getLength();i++)
+            this.physics.moveToObject(enemyGroup.getChildren()[i],player,300, this)
             },
             callbackScope :this,
             loop : true,
@@ -68,13 +71,21 @@ class GameScene extends Phaser.Scene {
             // repeat : 9
         })
    
-    
+        //Collision check
+        function touchingEnemy(player, enemyGroup) {
+            enemyGroup.disableBody(true, true);
+            enemyGroup.destroy();
+        }
+
+        this.physics.add.overlap(player, enemyGroup, touchingEnemy, null, this)
+        this.physics.add.overlap(enemyGroup,player, ()=> { 
+             enemyGroup.destroy();
+             console.log("hit")
+            
+        }, this)
     }
-           
 
-
-
-    update(delta, time) {
+    update(delta, time){
         if (this.keyA.isDown) {
             player.moveLeft();
         }
@@ -85,7 +96,6 @@ class GameScene extends Phaser.Scene {
             player.body.velocity.y = 0;
         }
 
-        
     }
 
     
