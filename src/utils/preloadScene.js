a84267a9339202c0c806615026463f7bc41365e7
NongFai <narisara.li@mail.kmutt.ac.th>
2020-01-09 18:30:59 +0700

    [UPDATE] preloading

--- src/utils/preloadScene.js
+++ src/utils/preloadScene.js
@@ -7,16 +7,17 @@ export const preloadScene = (data) => {
 }
 
 const defaultPreload = (scene) => {
-    let progressBar = scene.add.graphics();
+    
     let progressBox = scene.add.graphics();
-    progressBox.fillStyle(0x222222, 0.8);
+    let progressBar = scene.add.graphics();
+    progressBox.fillStyle(0xffffff,1);
     progressBox.fillRect(240, 270, 320, 50);
 
     let width = scene.cameras.main.width;
     let height = scene.cameras.main.height;
     let loadingText = scene.make.text({
-        x: width / 2,
-        y: height / 2 - 50,
+        x: 295,
+        y: 250,
         text: 'Loading...',
         style: {
             font: '20px monospace',
@@ -26,8 +27,8 @@ const defaultPreload = (scene) => {
     loadingText.setOrigin(0.5, 0.5);
 
     let percentText = scene.make.text({
-        x: width / 2,
-        y: height / 2 - 5,
+        x: 550,
+        y: 250,
         text: '0%',
         style: {
             font: '18px monospace',
@@ -50,7 +51,7 @@ const defaultPreload = (scene) => {
     scene.load.on('progress', function (value) {
         percentText.setText(parseInt(value * 100) + '%');
         progressBar.clear();
-        progressBar.fillStyle(0xffffff, 1);
+        progressBar.fillStyle(0x000000, 1);
         progressBar.fillRect(250, 280, 300 * value, 30);
     });
 
@@ -66,4 +67,5 @@ const defaultPreload = (scene) => {
         percentText.destroy();
         assetText.destroy();
     });
+
 }
